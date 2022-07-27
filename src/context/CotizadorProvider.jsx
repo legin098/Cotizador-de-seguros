c466c606a2388a262c765ce10d8a38f8b13f8e42
Jesus Cruz <jesus.cruz@campusvirtual.aunarvillavicencio.edu.co>
2022-07-27 17:51:02 -0500

    first commit

---
+++ src/context/CotizadorProvider.jsx
@@ -0,0 +1,75 @@
+import { createContext, useState } from "react";
+import {
+  calcularMarca,
+  calcularPlan,
+  formatearDinero,
+  obtenerDiferenciaYear,
+} from "../helpers";
+
+const CotizadorContext = createContext();
+
+const CotizadorProvider = ({ children }) => {
+  const [datos, setDatos] = useState({
+    marca: "",
+    year: "",
+    plan: "",
+  });
+
+  const [error, setError] = useState("");
+  const [resultado, setResultado] = useState(0);
+  const [cargando, setCargando] = useState(false);
+
+  const handleChangeDatos = (e) => {
+    setDatos({
+      ...datos,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const cotizarSeguro = () => {
+    //una base
+    let resultado = 2000;
+    //diferencia de anos
+    const diferencia = obtenerDiferenciaYear(datos.year);
+
+    //hay que restar el 3% por cada ano
+    resultado -= (diferencia * 3 * resultado) / 100;
+    //americano 15%
+    //europeo 30%
+    //asiatico 5%
+    resultado *= calcularMarca(datos.marca);
+    //basico 20%
+    //completo 50%
+    resultado *= calcularPlan(datos.plan);
+
+    //formatear dinero
+    resultado = formatearDinero(resultado);
+
+    setCargando(true);
+
+    setTimeout(() => {
+      setResultado(resultado);
+      setCargando(false);
+    }, 3000);
+  };
+
+  return (
+    <CotizadorContext.Provider
+      value={{
+        datos,
+        handleChangeDatos,
+        error,
+        setError,
+        cotizarSeguro,
+        resultado,
+        cargando
+      }}
+    >
+      {children}
+    </CotizadorContext.Provider>
+  );
+};
+
+export { CotizadorProvider };
+
+export default CotizadorContext;
